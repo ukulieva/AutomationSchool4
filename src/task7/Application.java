741a92532890b1603448f16c3c11ffd6f4cd90ea
juliakul <juliakul@e470-KulievaJ.playtika.local>
2019-10-17 01:24:37 +0300

    Add task7

---
+++ src/task7/Application.java
@@ -0,0 +1,83 @@
+package task7;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		double newN1 = 0;
+		double newN2 = 0;
+		Scanner number = new Scanner(System.in);
+
+		while (true) {
+
+			System.out.print("Input a first number: ");
+			String n1 = number.next();
+			if ("quit".equals(n1)) {
+				break;
+			}
+			try {
+				newN1 = Double.parseDouble(n1);
+			} catch (NumberFormatException e) {
+				System.out.println("Please, input correct number.");
+				continue;
+			}
+
+			System.out.print("Input sign: ");
+			String sign = number.next();
+			if ("quit".equals(sign))
+				break;
+			double result;
+			switch (sign) {
+			case ("-"):
+			case ("+"):
+			case ("*"):
+			case ("/"):
+				break;
+			default:
+				System.out.println("Please, input correct sign: - + * /");
+				continue;
+			}
+
+			System.out.print("Input a second number: ");
+			String n2 = number.next();
+			if ("quit".equals(n2))
+				break;
+			try {
+				newN2 = Double.parseDouble(n2);
+			} catch (NumberFormatException e) {
+				System.out.println("Please, input correct number.");
+				continue;
+			}
+
+			switch (sign) {
+			case ("-"):
+				result = newN1 - newN2;
+				System.out.println("Answer: " + result);
+				break;
+			case ("+"):
+				result = newN1 + newN2;
+				System.out.println("Answer: " + result);
+				break;
+			case ("*"):
+				result = newN1 * newN2;
+				System.out.println("Answer: " + result);
+				break;
+			case ("/"):
+				
+			if (newN2==0) {
+					System.out.println("Сann't be divided by 0.");
+					continue;
+				}
+				result = newN1 / newN2;
+				System.out.println("Answer: " + result);
+				break;
+			}
+			continue;
+
+		}
+		number.close();
+	}
+
+}
